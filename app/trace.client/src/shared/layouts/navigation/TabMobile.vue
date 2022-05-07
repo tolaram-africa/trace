49f78ddb3ebe1d6aa9efb6ee4d423e15276d4cd6
radioActive DROID <me@godwin.dev>
2022-05-07 05:03:11 +0100

    Improve tab modal

--- app/trace.client/src/shared/layouts/navigation/TabMobile.vue
+++ app/trace.client/src/shared/layouts/navigation/TabMobile.vue
@@ -5,16 +5,17 @@ import { useQuasar } from 'quasar';
 import { storeToRefs } from 'pinia';
 import { useLayoutStore } from 'src/shared/layouts/stores';
 import ModuleDialog from 'src/shared/layouts/navigation/ModuleDialog.vue';
+import BottomSheet from './BottomSheet.vue';
 import BottomSheetList from './BottomSheetList.vue';
 
 const $q = useQuasar();
-const swipeState = ref(false);
+const swipeModalState = ref(false);
 const layoutStore = useLayoutStore();
 const { setModuleDialogState } = layoutStore;
 const { moduleDialogState } = storeToRefs(layoutStore);
 
 const showModule = () => {
-  swipeState.value = !swipeState.value;
+  swipeModalState.value = !swipeModalState.value;
   return;
   if (!moduleDialogState.value) {
     setModuleDialogState(true);
@@ -48,6 +49,7 @@ export default {
       align="justify"
       no-caps
       switch-indicator
+      narrow-indicator
       indicator-color="action"
       content-class="mobile-footer-item"
       class="text-space"
@@ -69,18 +71,17 @@ export default {
       </q-route-tab>
       <q-route-tab name="more" icon="bi-grid-3x3-gap" @click="showModule" />
     </q-tabs>
-    <bottom-sheet-list
-      v-model:visible="swipeState"
-      :items="appMobileMenuExtended"
-      :threshold="150"
-    />
+    <bottom-sheet v-model:visible="swipeModalState" :threshold="150">
+      <bottom-sheet-list
+        @update:visible="swipeModalState = false"
+        :items="appMobileMenuExtended"
+      />
+    </bottom-sheet>
   </q-footer>
 </template>
 
 <style lang="scss" scoped>
 .app-footer {
-  padding-bottom: constant(safe-area-inset-bottom);
-  padding-bottom: env(safe-area-inset-bottom);
   border-radius: $border-radius-lg $border-radius-lg 0 0;
   background-color: var(--q-app-plain);
 
