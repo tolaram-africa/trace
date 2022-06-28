8eee6bcbbbbf96b2ecec99e0b88af1c567dc29b0
radioActive DROID <me@godwin.dev>
2022-06-28 15:54:11 +0100

    Update ormconfig.ts

--- app/trace.service/ormconfig.ts
+++ app/trace.service/ormconfig.ts
@@ -3,8 +3,16 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import 'dotenv/config';
 
+export declare type DriverType =
+  | 'mysql'
+  | 'postgres'
+  | 'cockroachdb'
+  | 'mariadb'
+  | 'mssql';
+
 export const ormConfig: DataSourceOptions = {
-  type: 'postgres',
+  name: 'default',
+  type: (String(process.env.POSTGRES_DB_TYPE) as DriverType) || 'postgres',
   host: process.env.POSTGRES_DB_HOST,
   port: parseInt(<string>process.env.POSTGRES_DB_PORT),
   username: process.env.POSTGRES_USER,
@@ -12,9 +20,10 @@ export const ormConfig: DataSourceOptions = {
   database: process.env.POSTGRES_DB_NAME,
   entityPrefix: process.env.POSTGRES_TABLE_PREFIX || '',
   entities: [__dirname + '/src/model/**/*.entity.{ts,js}'],
-  migrations: [__dirname + '/src/migrations/*.{ts,js}'],
-  subscribers: [__dirname + '/src/subscriber/*.{ts,js}'],
+  migrations: ['/src/migrations/*.{ts,js}'],
+  subscribers: [],
   synchronize: true,
+  dropSchema: false,
   namingStrategy: new SnakeNamingStrategy(),
 };
 
@@ -27,9 +36,8 @@ export const OrmConfig = {
     entitiesDir: 'src/model',
   },
 };
-
-export const AppDataSource = new DataSource(OrmConfig);
-
-AppDataSource.initialize();
+const appDataSource = new DataSource(OrmConfig);
+appDataSource.initialize();
+export const AppDataSource = appDataSource;
 
 export default ormConfig;
