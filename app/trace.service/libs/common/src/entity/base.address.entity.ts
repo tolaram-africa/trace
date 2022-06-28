aabec1a1fefa441005ad4f509019a23254c367ee
radioActive DROID <me@godwin.dev>
2022-06-28 19:23:08 +0100

    Common update

---
+++ app/trace.service/libs/common/src/entity/base.address.entity.ts
@@ -0,0 +1,27 @@
+import { Column } from 'typeorm';
+
+export class ExtendedAddress {
+  @Column({ type: 'varchar', length: 1024, nullable: true })
+  public street!: string;
+
+  @Column({ type: 'varchar', length: 128, nullable: true })
+  public city!: string;
+
+  @Column({ type: 'varchar', length: 128, nullable: true })
+  public county!: string;
+
+  @Column({ type: 'varchar', length: 128, nullable: true })
+  public state!: string;
+
+  @Column({ type: 'varchar', length: 128, nullable: true })
+  public zip!: string;
+
+  @Column({ type: 'varchar', length: 128, nullable: true })
+  public country!: string;
+
+  @Column({ type: 'bigint', nullable: true })
+  public phone!: number;
+
+  @Column({ type: 'varchar', length: 256, nullable: true })
+  public email!: string;
+}
