8ef1a019511d74ce0c44dd2203209f5d822e6357
radioActive DROID <me@godwin.dev>
2022-07-07 00:09:21 +0100

    Update common.module.ts

--- app/trace.service/libs/common/src/common.module.ts
+++ app/trace.service/libs/common/src/common.module.ts
@@ -1,24 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ormConfig } from '@root/ormconfig';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { CommonService } from './common.service';
-import { CommonResolver } from './common.resolver';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(ormConfig),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      installSubscriptionHandlers: true,
-      driver: ApolloDriver,
-      // TODO: Set option for persisted schema file.
-      autoSchemaFile: true,
-      debug: true,
-      playground: true,
-    }),
-  ],
-  providers: [CommonService, CommonResolver],
+  imports: [],
+  providers: [CommonService],
   exports: [CommonService],
 })
 export class CommonModule {}
