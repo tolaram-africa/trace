19cdb7b3decf5ee9da2208a05f3fc6c4f0ca1c84
radioActive DROID <me@godwin.dev>
2022-07-10 04:50:43 +0100

    AdminJS dashboard temp change

--- app/trace.service/apps/manager/src/admin/dashboard.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const Dashboard = (props) => {
-  return <h2>Trace Admin</h2>;
-};
-
-export default Dashboard;
