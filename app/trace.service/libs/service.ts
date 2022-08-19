6fc42825e7333f02afaa0f1eafd9faf9b377d3cd
radioActive DROID <me@godwin.dev>
2022-08-19 16:44:39 +0100

    Improve service bootstrap functions

--- app/trace.service/libs/service.ts
+++ app/trace.service/libs/service.ts
@@ -8,7 +8,7 @@ import { ConfigService } from '@nestjs/config';
 import { IServiceConfig, PROD_ENV, SERVICE_PROFILE } from './config';
 import { graphqlUploadExpress } from 'graphql-upload';
 
-interface IServiceInstance {
+export interface IServiceInstance {
   name: string;
   host: string;
   port: number;
@@ -19,48 +19,62 @@ interface IServiceInstance {
 
 export const getServiceInstance = (
   app: INestApplication,
-  name: SERVICE_PROFILE,
+  profile: SERVICE_PROFILE,
 ): IServiceInstance => {
   const configService = app.get<ConfigService>(ConfigService);
-  const config = configService.getOrThrow<IServiceConfig>('services.' + name);
-  const logMode = (
-    PROD_ENV
-      ? ['log', 'warn', 'error']
-      : ['debug', 'log', 'warn', 'error', 'verbose']
+  const config = configService.getOrThrow<IServiceConfig>(
+    `services.${profile}`,
+  );
+  const PROD_LOG_LEVEL = ['log', 'warn', 'error'];
+  const DEV_LOG_LEVEL = ['debug', 'log', 'warn', 'error', 'verbose'];
+  const { name, host, port } = config;
+  const LOG_MODE = (
+    PROD_ENV ? PROD_LOG_LEVEL : DEV_LOG_LEVEL
   ) as Array<LogLevel>;
   const logOptions =
-    configService.getOrThrow<Array<LogLevel>>('app.logLevel') || logMode;
+    configService.getOrThrow<Array<LogLevel>>('app.logLevel') || LOG_MODE;
   app.useLogger(logOptions);
   app.useGlobalPipes(new ValidationPipe());
   const logger = new Logger('bootstrap');
-
-  /* Try starting up server. */
-  logger.log(`Starting service ${config.name}...`);
+  // logger.log(`Starting service ${name}...`);
   return {
-    name: config.name,
-    host: config.host,
-    port: config.port,
+    name,
+    host,
+    port,
     app,
-    url: `http://${config.host}:${config.port}`,
+    url: `http://${host}:${port}`,
     logger,
   };
+};
+
+const defaultStartExec = (ctx: IServiceInstance) => {
+  ctx.logger.log(`Starting service ${ctx.name}...`);
+  return ctx;
+};
+
+const defaultEndExec = (ctx: IServiceInstance) => {
+  const { name, logger, url } = ctx;
+  logger.log('Service startup completed...');
+  logger.log(`${name.toUpperCase()} service is listening on ${url}/`);
+  return ctx;
 };
 
 export const bootstrapService = async (
   context: IServiceInstance,
   optinalContext = true,
+  endExec: (ctx: IServiceInstance) => void = defaultEndExec,
+  startExec: (ctx: IServiceInstance) => IServiceInstance = defaultStartExec,
 ): Promise<void> => {
-  const { app, name, host, port, logger, url } = context;
+  const { app, host, port, logger } = startExec(context);
   if (optinalContext) {
     app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }));
     app.enableCors({
       exposedHeaders: 'X-Document-Name',
     });
-  }
+  } else app.enableCors();
   try {
     await app.listen(port, host, () => {
-      logger.log('Service startup completed...');
-      logger.log(`${name.toUpperCase()} service is listening on ${url}/`);
+      endExec(context);
     });
   } catch (error) {
     logger.error('A Fatal error occurred', error);
