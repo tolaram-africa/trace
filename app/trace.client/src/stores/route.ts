b319d6dff9fb8c9eac7ea2810d093611c0ea4c07
radioActive DROID <me@godwin.dev>
2023-02-17 14:07:09 +0100

    Added ui route store state

---
+++ app/trace.client/src/stores/route.ts
@@ -0,0 +1,43 @@
+import { ref } from 'vue';
+import { defineStore } from 'pinia';
+import { ServiceVariant, ITenantProfile } from '@/shared/libs/Service';
+
+export const useRouteStore = defineStore(
+  'route',
+  () => {
+    /** States */
+    const tenant = ref<ITenantProfile>();
+    const services = ref<Array<ServiceVariant>>([ServiceVariant.VECTOR]);
+    const service = ref<ServiceVariant>(services.value[0]);
+
+    const getTenant = () => tenant.value;
+
+    const setTenant = (payload: ITenantProfile) => {
+      tenant.value = payload;
+    };
+
+    const getPermittedServices = () => services;
+
+    const setPermittedServices = (svs: Array<ServiceVariant>): void => {
+      services.value = svs;
+    };
+
+    const getService = (): ServiceVariant => service.value;
+
+    const setService = (sv: ServiceVariant): void => {
+      service.value = sv;
+    };
+
+    return {
+      getTenant,
+      setTenant,
+      getPermittedServices,
+      setPermittedServices,
+      getService,
+      setService,
+    };
+  },
+  {
+    persist: true,
+  }
+);
