4918cf18cde89437d35b94867e17a18d3dad423e
radioActive DROID <me@godwin.dev>
2023-03-12 19:42:57 +0100

    setup Trace.Service.Track GraphQL

--- src/Service/Track/Program.cs
+++ src/Service/Track/Program.cs
@@ -1,6 +1,37 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using HotChocolate;
+using HotChocolate.Types;
+using StackExchange.Redis;
+using Trace.Common.Service;
+using Trace.Service.Track;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+.AddAuthorization()
+.RegisterRedis(builder.Configuration);
+
+builder.Services
+.AddMemoryCache()
+.AddGraphQLServer()
+.AddTraceDefaults()
+.PublishSchemaDefinition(c => {
+    c.SetName(Nodes.Track)
+    .PublishToRedis(Nodes.GroupName, sp => sp.GetRequiredService<ConnectionMultiplexer>());
+})
+.AddType<UploadType>()
+.AddQueryType<Query>()
+.AddSpatialTypes()
+.AddSpatialFiltering()
+.AddSpatialProjections()
+.AddQueryableCursorPagingProvider()
+.RegisterObjectExtensions(typeof(Program).Assembly);
+
+
+var app = builder.Build();
+app.MapGet("/", () => "Service.Track");
+app.UseRouting();
+app.UseAuthorization();
+app.UseWebSockets();
+app.MapGraphQL();
 
 app.Run();
---
+++ src/Service/Track/Query.cs
@@ -0,0 +1,5 @@
+namespace Trace.Service.Track;
+
+public class Query {
+    public int GetCheck() => 1;
+}
