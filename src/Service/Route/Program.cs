e4938ab634a20dede4c6fbd089f70b037e5b24f3
radioActive DROID <me@godwin.dev>
2023-03-12 19:43:10 +0100

    setup Trace.Service.Route GraphQL

--- src/Service/Route/Program.cs
+++ src/Service/Route/Program.cs
@@ -1,6 +1,37 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using HotChocolate;
+using HotChocolate.Types;
+using StackExchange.Redis;
+using Trace.Common.Service;
+using Trace.Service.Route;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+.AddAuthorization()
+.RegisterRedis(builder.Configuration);
+
+builder.Services
+.AddMemoryCache()
+.AddGraphQLServer()
+.AddTraceDefaults()
+.PublishSchemaDefinition(c => {
+    c.SetName(Nodes.Route)
+    .PublishToRedis(Nodes.GroupName, sp => sp.GetRequiredService<ConnectionMultiplexer>());
+})
+.AddType<UploadType>()
+.AddQueryType<Query>()
+.AddSpatialTypes()
+.AddSpatialFiltering()
+.AddSpatialProjections()
+.AddQueryableCursorPagingProvider()
+.RegisterObjectExtensions(typeof(Program).Assembly);
+
+
+var app = builder.Build();
+app.MapGet("/", () => "Service.Route");
+app.UseRouting();
+app.UseAuthorization();
+app.UseWebSockets();
+app.MapGraphQL();
 
 app.Run();
