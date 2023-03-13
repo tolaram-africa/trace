2311f0198b9198bb5f3ae420014692009a2dd39c
radioActive DROID <me@godwin.dev>
2023-03-13 17:31:05 +0100

    Update BeaconDevice.cs

--- src/Common/Domain/Modules/Asset/Entities/BeaconDevice.cs
+++ src/Common/Domain/Modules/Asset/Entities/BeaconDevice.cs
@@ -6,13 +6,5 @@ namespace Trace.Common.Domain.Modules.Asset.Entities;
 public sealed class BeaconDevice : AssetEntity {
     public BeaconDeviceStatus Status { get; set; }
 
-    public string UniqueId { get; set; }
-
     public DateTime? LastUpdate { get; set; }
-
-    public string Color { get; set; }
-
-    public DateTime? Deployed { get; set; }
-
-    public DateTime? Decommissioned { get; set; }
 }
