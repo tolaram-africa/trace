4abd87679243f3b7d151c9e5f8bcc11e7b69e472
radioActive DROID <me@godwin.dev>
2023-03-17 22:03:05 +0100

    Update Location.cs

--- src/Common/Domain/Modules/Location/Entities/Location.cs
+++ src/Common/Domain/Modules/Location/Entities/Location.cs
@@ -4,15 +4,15 @@ using Trace.Common.Domain.Modules.Location.Enum;
 namespace Trace.Common.Domain.Modules.Location.Entities;
 
 public class Location : TenantEntity<Guid> {
-    public string OsmId { get; set; }
+    public string? OsmId { get; set; }
     
     public bool Default { get; set; }
 
     public bool Custom { get; set; }
 
-    public string Name { get; set; }
+    public string Name { get; set; } = String.Empty;
     
-    public string Address { get; set; }
+    public string? Address { get; set; }
 
     public DateTime? ApprovedAt { get; set; }
     
@@ -22,7 +22,7 @@ public class Location : TenantEntity<Guid> {
 
     public LocationCategory? Category { get; set; }
     
-    public Geometry Shape { get; set; }
+    public Geometry Shape { get; set; } = null!;
 
     public string Description { get; set; } = String.Empty;
 }
