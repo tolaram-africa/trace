9c3591aed7fc71fcd70fe0749effc24b05faf1f7
radioActive DROID <me@godwin.dev>
2023-03-26 21:20:23 +0100

    Added some base context partial classes

---
+++ src/Common/Domain/Modules/Tag/BaseContext.Partial.cs
@@ -0,0 +1,19 @@
+// Copyright 2022 - 2023 Godwin peter .O (me@godwin.dev)
+// 
+// Licensed under the Reciprocal Public License (RPL-1.5) and Trace License;
+// you may not use this file except in compliance with the License.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Microsoft.EntityFrameworkCore;
+using Trace.Common.Domain.Modules.Tag.Entities;
+
+namespace Trace.Common.Domain.Modules;
+
+public abstract partial class BaseContext  {
+    public DbSet<Tag.Entities.Tag> Tags { get; set; } = default!;
+    public DbSet<TagMember> TagMembers { get; set; } = default!;
+}
